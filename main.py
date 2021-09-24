d5276a3198e33143f4a50796fc67c4d802ea49da
syorkp <sampinky1999@gmail.com>
2021-09-24 13:58:57 +0100

    SBPPO wth minibatches

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # import gym
 
 from stable_baselines.common.policies import MlpLstmPolicy
-# from stable_baselines.common import make_vec_env
+from stable_baselines.common import make_vec_env
 from stable_baselines import PPO2
 #from stable_baselines.common.env_checker import check_env
 from policy import CustomPolicy
@@ -11,11 +11,11 @@ from policy import CustomPolicy
 from environment import CustomEnv
 
 # multiprocess environment
-# env = make_vec_env('CartPole-v1', n_envs=4)
-env = CustomEnv(rendering_frequency=100)
+env = make_vec_env(CustomEnv, n_envs=8, env_kwargs={"rendering_frequency": 200})
+# env = CustomEnv(rendering_frequency=100)
 # check_env(env)
 
-model = PPO2(CustomPolicy, env, n_steps=1000, full_tensorboard_log=False, nminibatches=1, tensorboard_log='./ppo_tensorboard/')#, policy_kwargs={"data_format":"NCHW"})
+model = PPO2(CustomPolicy, env, n_steps=1000, full_tensorboard_log=False, nminibatches=4, tensorboard_log='./ppo_tensorboard/')#, policy_kwargs={"data_format":"NCHW"})
 # model = PPO2.load("ppo2_cartpole", env=env)
 
 model.learn(total_timesteps=2500000)#, callback=TensorboardCallback())
