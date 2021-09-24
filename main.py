1fbb083d788e0a948552ac67dc1ec7c674bb3594
syorkp <sampinky1999@gmail.com>
2021-09-24 18:56:23 +0100

    Ready for DiscreteReflected

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from Environments.continuous_environment import ContinuousEnv
 from Environments.discrete_environment import DiscreteEnv
 
 
-trial_name = "ContinuousReflected"
+trial_name = "DiscreteReflected"
 
 if not os.path.exists(f"Output/{trial_name}/"):
     os.makedirs(f"Output/{trial_name}/")
@@ -25,7 +25,7 @@ if not os.path.exists(f"Output/{trial_name}/"):
 checkpoint_callback = CheckpointCallback(save_freq=100000, save_path=f'./Output/{trial_name}/model_checkpoints/')
 env_log_callback = TensorboardCallback()
 
-env = make_vec_env(ContinuousEnv, n_envs=8, env_kwargs={"rendering_frequency": 200, "trial_name": trial_name})
+env = make_vec_env(DiscreteEnv, n_envs=8, env_kwargs={"rendering_frequency": 200, "trial_name": trial_name})
 
 model = PPO2(ReflectedPolicy, env, n_steps=1000, full_tensorboard_log=False, nminibatches=4, tensorboard_log=f'./Output/{trial_name}/ppo_tensorboard/')#, policy_kwargs={"data_format":"NCHW"})
 
