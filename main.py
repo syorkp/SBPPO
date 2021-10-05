3d7275efb07d7763621fdbf7c44ff2ba56bb9bd8
syorkp <sampinky1999@gmail.com>
2021-10-05 11:34:14 +0100

    Potential fixes, updating file name

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 from stable_baselines.common import make_vec_env
 from stable_baselines import PPO2
+from stable_baselines.common.policies import LstmPolicy
 from stable_baselines.common.callbacks import CheckpointCallback
 
 from additional_logging import LoggingCallback, SavingCallback
@@ -15,41 +16,42 @@ import tensorflow as tf
 
 trial_name = "ContinuousReflectedSaving"
 
-if not os.path.exists(f"Output/{trial_name}/"):
-    os.makedirs(f"Output/{trial_name}/")
-    os.makedirs(f'Output/{trial_name}/model_checkpoints/')
-    os.makedirs(f'Output/{trial_name}/ppo_tensorboard/')
-    os.makedirs(f'Output/{trial_name}/episodes/')
+if not os.path.exists(f"Training-Output/{trial_name}/"):
+    os.makedirs(f"Training-Output/{trial_name}/")
+    os.makedirs(f'Training-Output/{trial_name}/model_checkpoints/')
+    os.makedirs(f'Training-Output/{trial_name}/ppo_tensorboard/')
+    os.makedirs(f'Training-Output/{trial_name}/episodes/')
 
 
 # Callbacks for saving
-# checkpoint_callback = CheckpointCallback(save_freq=100000, save_path=f'./Output/{trial_name}/model_checkpoints/')
+# checkpoint_callback = CheckpointCallback(save_freq=100000, save_path=f'./Training-Output/{trial_name}/model_checkpoints/')
 # env_log_callback = TensorboardCallback()
 
 env = make_vec_env(ContinuousEnv, n_envs=8, env_kwargs={"rendering_frequency": 100, "trial_name": trial_name})
-model = PPO2(ReflectedPolicy, env, n_steps=1000, full_tensorboard_log=False, nminibatches=4, tensorboard_log=f'./Output/{trial_name}/ppo_tensorboard/')#, policy_kwargs={"data_format":"NCHW"})
+model = PPO2(ReflectedPolicy, env, n_steps=1000, full_tensorboard_log=False, nminibatches=4,
+             tensorboard_log=f'./Training-Output/{trial_name}/ppo_tensorboard/')#, policy_kwargs={"data_format":"NCHW"})
 
 model.learn(total_timesteps=100000)#, callback=saving_callback)#, callback=[env_log_callback, checkpoint_callback])
 saver = tf.train.Saver(max_to_keep=10)
-saver.save(model.sess, f"./Output/{trial_name}/model_checkpoints/model-100000.cptk")
+saver.save(model.sess, f"./Training-Output/{trial_name}/model_checkpoints/model-100000.cptk")
 
 model.learn(total_timesteps=100000)
-saver.save(model.sess, f"./Output/{trial_name}/model_checkpoints/model-200000.cptk")
+saver.save(model.sess, f"./Training-Output/{trial_name}/model_checkpoints/model-200000.cptk")
 
 model.learn(total_timesteps=100000)
-saver.save(model.sess, f"./Output/{trial_name}/model_checkpoints/model-300000.cptk")
+saver.save(model.sess, f"./Training-Output/{trial_name}/model_checkpoints/model-300000.cptk")
 
 model.learn(total_timesteps=100000)
-saver.save(model.sess, f"./Output/{trial_name}/model_checkpoints/model-400000.cptk")
+saver.save(model.sess, f"./Training-Output/{trial_name}/model_checkpoints/model-400000.cptk")
 
 model.learn(total_timesteps=100000)
-saver.save(model.sess, f"./Output/{trial_name}/model_checkpoints/model-500000.cptk")
+saver.save(model.sess, f"./Training-Output/{trial_name}/model_checkpoints/model-500000.cptk")
 
 model.learn(total_timesteps=100000)
-saver.save(model.sess, f"./Output/{trial_name}/model_checkpoints/model-600000.cptk")
+saver.save(model.sess, f"./Training-Output/{trial_name}/model_checkpoints/model-600000.cptk")
 
 model.learn(total_timesteps=100000)
-saver.save(model.sess, f"./Output/{trial_name}/model_checkpoints/model-700000.cptk")
+saver.save(model.sess, f"./Training-Output/{trial_name}/model_checkpoints/model-700000.cptk")
 
 # model = PPO2.load("ppo_simfish")
 
