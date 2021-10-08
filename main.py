3bfa4807086cbe9285bc1514f519b3b508a5e2dd
syorkp <sampinky1999@gmail.com>
2021-10-08 17:34:40 +0100

    Test without multiple envs

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from Environments.discrete_environment import DiscreteEnv
 
 import tensorflow as tf
 
-trial_name = "DiscreteReflected3"
+trial_name = "Test"
 
 if not os.path.exists(f"Training-Output/{trial_name}/"):
     os.makedirs(f"Training-Output/{trial_name}/")
@@ -27,8 +27,8 @@ if not os.path.exists(f"Training-Output/{trial_name}/"):
 # checkpoint_callback = CheckpointCallback(save_freq=100000, save_path=f'./Training-Output/{trial_name}/model_checkpoints/')
 # env_log_callback = TensorboardCallback()
 
-env = make_vec_env(DiscreteEnv, n_envs=8, env_kwargs={"rendering_frequency": 100, "trial_name": trial_name})
-model = PPO2(ReflectedPolicy, env, n_steps=1000, full_tensorboard_log=False, nminibatches=4,
+env = make_vec_env(ContinuousEnv, n_envs=1, env_kwargs={"rendering_frequency": 100, "trial_name": trial_name})
+model = PPO2(ReflectedPolicy, env, n_steps=1000, full_tensorboard_log=False, nminibatches=1,
              tensorboard_log=f'./Training-Output/{trial_name}/ppo_tensorboard/')#, policy_kwargs={"data_format":"NCHW"})
 
 model.learn(total_timesteps=2000000)#, callback=saving_callback)#, callback=[env_log_callback, checkpoint_callback])
